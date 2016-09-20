0bc5e233ed97ae035878e2b7b39265694d2cd327
lakshay girdhar <lakshay148@gmail.com>
2016-09-20 16:38:40 +0530

    Changes for GA version

--- Neon/src/main/java/com/scanlibrary/ImageReviewActivity.java
+++ Neon/src/main/java/com/scanlibrary/ImageReviewActivity.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.ViewPager;
@@ -17,9 +16,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.gaadi.neon.util.FileInfo;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 
@@ -45,7 +41,6 @@ public class ImageReviewActivity extends AppCompatActivity implements View.OnCli
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    private GoogleApiClient client;
     private UpdateImageListReceiver mUpdateImageListReceiver;
 //    private boolean singleTagSelection;
 
@@ -142,7 +137,6 @@ public class ImageReviewActivity extends AppCompatActivity implements View.OnCli
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     ArrayList<ImageModel> tempImageList;
@@ -264,17 +258,6 @@ public class ImageReviewActivity extends AppCompatActivity implements View.OnCli
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "ImageReview Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.scanlibrary/http/host/path")
-        );
 //        AppIndex.AppIndexApi.start(client, viewAction);
     }
 
@@ -284,18 +267,6 @@ public class ImageReviewActivity extends AppCompatActivity implements View.OnCli
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "ImageReview Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.scanlibrary/http/host/path")
-        );
-//        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
     }
 
     /*  @Override
